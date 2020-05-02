815212e7a6f6b0ff6ff037a1f5892287256c83dc
Kami <1.dankon.1@gmail.com>
2020-05-02 22:20:57 +0300

    Tag migration and model

---
+++ server/migrations/20200502191804-create-tag.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Tags', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      uuid: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        unique: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize)
+    => queryInterface.dropTable('Tags'),
+};
---
+++ server/models/tag.js
@@ -0,0 +1,32 @@
+import uuid from 'uuid/v4';
+
+export default (sequelize, DataTypes) => {
+  const Tag = sequelize.define('Tag', {
+    uuid: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isUUID: 4,
+      },
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter name tag',
+      },
+      unique: {
+        args: true,
+        msg: 'Name tag is already taken',
+      },
+    },
+  }, {});
+
+  Tag.associate = models => {
+    // associations can be defined here
+  };
+
+  Tag.beforeCreate(tag => tag.uuid = uuid());
+  return Tag;
+};
