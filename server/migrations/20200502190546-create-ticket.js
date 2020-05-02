f7b0bec9adc8c6f9aa2778295704a9fbc504d270
Kami <1.dankon.1@gmail.com>
2020-05-02 22:09:17 +0300

    Ticket migration and model

---
+++ server/migrations/20200502190546-create-ticket.js
@@ -0,0 +1,53 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Tickets', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      uuid: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        unique: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      count: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      price: {
+        type: Sequelize.INTEGER
+      },
+      datetimeTo: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      datetimeFrom: {
+        type: Sequelize.DATE
+      },
+      eventId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Tickets');
+  }
+};
---
+++ server/models/ticket.js
@@ -0,0 +1,51 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Ticket = sequelize.define('Ticket', {
+    uuid: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isUUID: 4,
+      },
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter event name',
+      },
+      validate: {
+        len: {
+          args: [0, 100],
+          msg: 'Event name must be less than 100',
+        },
+      },
+    },
+    description: DataTypes.TEXT,
+    count: {
+      type: DataTypes.INTEGER,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the cont of participants in the event',
+      },
+      defaultValue: 0,
+    },
+    price: DataTypes.INTEGER,
+    datetimeTo: {
+      type: DataTypes.DATE,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the event start date',
+      },
+    },
+    datetimeFrom: DataTypes.DATE,
+    eventId: {
+      type: DataTypes.INTEGER,
+    },
+  }, {});
+  Ticket.associate = function(models) {
+    // associations can be defined here
+  };
+  return Ticket;
+};
