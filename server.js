40caf2ff511c8252662c29fd4339efcd585b3f50
Kami <1.dankon.1@gmail.com>
2020-05-06 13:07:23 +0300

    change https on http

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-import https from 'https';
+import http from 'http';
 import app from './app';
 
 const HOSTNAME = '127.0.0.1';
 const PORT = '5000';
 
-const server = https.createServer(app);
+const server = http.createServer(app);
 server.listen(PORT, HOSTNAME, () => {
-  console.log(`Server running at https://${HOSTNAME}:${PORT}/`);
+  console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
 });
