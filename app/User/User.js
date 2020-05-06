747c03a3b4faa94fb7bcc1c1fc6b3b6313ff0107
Kami <1.dankon.1@gmail.com>
2020-05-06 12:20:48 +0300

    discribe User class

---
+++ app/User/User.js
@@ -0,0 +1,14 @@
+import Model from '../Base/Model';
+import models from '../../db/models';
+const {
+  AuthorizedUser: AuthorizedUserModel,
+  User: UserModel,
+} = models;
+
+class User extends Model {
+  constructor() {
+    super(UserModel);
+  }
+};
+
+export default User;
