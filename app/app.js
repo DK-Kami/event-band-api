9e598c5e203ca2642d28d3f051a09cd50ec2b21a
Kami <1.dankon.1@gmail.com>
2020-05-06 12:20:37 +0300

    app module

---
+++ app/app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+import routes from './routes';
+
+const app = express();
+
+const allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Authorization, Content-Type, Accept");
+  next();
+};
+app.use(allowCrossDomain);
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(routes);
+
+export default app;
