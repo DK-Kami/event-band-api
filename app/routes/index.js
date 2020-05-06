7f4cbe1e76d6eeb304f6e57e3d7141cfe1e27974
Kami <1.dankon.1@gmail.com>
2020-05-06 12:22:46 +0300

    main router

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,12 +1,12 @@
-import Users from '../controllers/user';
-import AuthorizedUser from '../controllers/authorizedUser';
+const mainRouter = require('../Base/Router')();
 
-export default (app) => {
-  app.get('/api',
-    (req, res) => res.status(200).send({ message: 'Welcome to the huita API!', })
-  );
-  app.get('/api/users', Users.getAll);
-  app.get('/api/users/:id', Users.getCurrent);
-  
-  // API route for user to signup
-};
+mainRouter.use('/api',  require('./privateRouter'));
+mainRouter.use('/',     require('./publicRouter'));
+
+mainRouter.use('*', (req, res) => {
+  res.status(500).send({
+    message: 'method not implemented',
+  });
+});
+
+export default mainRouter;
