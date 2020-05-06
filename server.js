48c96ac51f6bd9b7869f269bac7143dd83ccdc63
Kami <1.dankon.1@gmail.com>
2020-05-06 12:11:34 +0300

    file to server run

--- server.js
+++ server.js
@@ -1,24 +1,10 @@
-import http from 'http';
-import express from 'express';
-import logger from 'morgan';
-import bodyParser from 'body-parser';
-import routes from './server/routes';
+import https from 'https';
+import app from './app';
 
-const hostname = '127.0.0.1';
-const port = '5000';
+const HOSTNAME = '127.0.0.1';
+const PORT = '5000';
 
-const app = express();
-
-const server = http.createServer(app);
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-routes(app);
-
-app.get('*', (req, res) => res.status(200).send({
-  message: 'Welcome to the default API route',
-}));
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+const server = https.createServer(app);
+server.listen(PORT, HOSTNAME, () => {
+  console.log(`Server running at https://${HOSTNAME}:${PORT}/`);
 });
