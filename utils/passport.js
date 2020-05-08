1605b19d4ac7003a05f09da1078e2f8a0d4ec284
Kami <1.dankon.1@gmail.com>
2020-05-08 16:42:13 +0300

    passport config

---
+++ utils/passport.js
@@ -0,0 +1,26 @@
+import LocalStrategy from 'passport-local';
+import passport from 'passport';
+import AuthorizedUser from '../app/User/AuthorizedUser';
+import User from '../app/User/User';
+
+const incorectError = {
+  message: 'Incorect email or password',
+};
+
+passport.use(new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+}, async (email, password, done) => {
+  const user = await User.getOne({ where: { email }});
+  if (!user) return done(incorectError);
+
+  const authUser = await user.getAuthorizedUser();
+
+  if (!authUser) return done(incorectError);
+
+  const passwordIsMatch = AuthorizedUser.cryptoPassword(password, authUser.salt) === authUser.password;
+  if (passwordIsMatch) {
+    return done(null, AuthorizedUser.toAuthJSON(authUser, user));
+  }
+  return done(incorectError);
+}));
