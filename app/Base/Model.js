da50980823fac41de1d8ab20d1d1ecf93d8c172a
Kami <1.dankon.1@gmail.com>
2020-05-10 01:28:49 +0300

    change model name

--- app/Base/Model.js
+++ app/Base/Model.js
@@ -1,30 +1,41 @@
 import { v4 } from 'uuid';
 
 class Model {
-  constructor(model) {
-    this.model = model;
+  constructor(Model) {
+    this.Model = Model;
   }
 
   async getAll(queryObject) {
-    const data = await this.model.findAll(queryObject);
+    const data = await this.Model.findAll(queryObject);
     return data;
   }
 
   async getById(id, raw = false) {
-    const data = await this.model.findByPk(id, { raw });
+    const data = await this.Model.findByPk(id, { raw });
     return data;
   }
 
   async getOne(queryObject) {
-    const data = await this.model.findOne(queryObject)
+    const data = await this.Model.findOne(queryObject)
     return data;
   }
 
   async create(createData) {
     createData.uuid = v4();
-    const data = await this.model.create(createData);
+    const data = await this.Model.create(createData);
     return data;
   }
+  // async create(createData) {
+  //   createData.uuid = v4();
+  //   console.log('createData', createData)
+  //   const newModel = this.Model.build(createData);
+  //   // console.log('newModel', newModel);
+  //   const isValid = await newModel.validate();
+  //   console.log('validate', isValid);
+  //   return newModel;
+  //   // const data = await this.Model.create(createData);
+  //   // return data;
+  // }
 };
 
 export default Model;
