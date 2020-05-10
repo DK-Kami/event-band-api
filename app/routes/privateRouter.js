248c56b5b6a689ea4f95accb4f20803e8e3f838b
Kami <1.dankon.1@gmail.com>
2020-05-11 00:53:55 +0300

    private and public routes

--- app/routes/privateRouter.js
+++ app/routes/privateRouter.js
@@ -1,7 +1,9 @@
+import { profileRouter } from '../User/profile';
 import { userRouter } from '../User';
 import Router from '../Base/Router';
 const router = new Router();
 
+router.use('/profile', profileRouter);
 router.use('/user', userRouter);
 
 export default router;
--- app/routes/publicRouter.js
+++ app/routes/publicRouter.js
@@ -1,7 +1,9 @@
+import { passwordRouter } from '../User/profile';
 import { authRouter } from '../User';
 import Router from '../Base/Router';
 const router = new Router();
 
+router.use('/', passwordRouter);
 router.use('/', authRouter);
 
 export default router;
