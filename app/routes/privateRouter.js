3d4679ba1f131596d1a30ba5c22c9e555afbbfbb
Kami <1.dankon.1@gmail.com>
2020-05-12 00:28:11 +0300

    add organisation and organizer routers to main router

--- app/routes/privateRouter.js
+++ app/routes/privateRouter.js
@@ -1,8 +1,12 @@
+import { organizationRouter } from '../Organization';
+import { organizerRouter } from '../Organizer';
 import { profileRouter } from '../User/profile';
 import { userRouter } from '../User';
 import Router from '../Base/Router';
 const router = new Router();
 
+router.use('/organization', organizationRouter);
+router.use('/organizer', organizerRouter);
 router.use('/profile', profileRouter);
 router.use('/user', userRouter);
 
