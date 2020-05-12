6251a01af3f809457149fe6fecf287cf7d07903e
Kami <1.dankon.1@gmail.com>
2020-05-12 18:22:46 +0300

    add tag routes to mainRouter

--- app/routes/privateRouter.js
+++ app/routes/privateRouter.js
@@ -3,6 +3,7 @@ import { organizerRouter } from '../Organizer';
 import { profileRouter } from '../User/profile';
 import { eventRouter } from '../Event';
 import { userRouter } from '../User';
+import { tagRouter } from '../Tag';
 import Router from '../Base/Router';
 const router = new Router();
 
@@ -11,5 +12,6 @@ router.use('/organizer', organizerRouter);
 router.use('/profile', profileRouter);
 router.use('/event', eventRouter);
 router.use('/user', userRouter);
+router.use('/tag', tagRouter);
 
 export default router;
