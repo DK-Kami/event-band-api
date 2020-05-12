ecc27baf8021cb6c8aefea054b1f4a7f52812c9c
Kami <1.dankon.1@gmail.com>
2020-05-12 04:21:22 +0300

    add event route to mainRouter

--- app/routes/privateRouter.js
+++ app/routes/privateRouter.js
@@ -1,6 +1,7 @@
 import { organizationRouter } from '../Organization';
 import { organizerRouter } from '../Organizer';
 import { profileRouter } from '../User/profile';
+import { eventRouter } from '../Event';
 import { userRouter } from '../User';
 import Router from '../Base/Router';
 const router = new Router();
@@ -8,6 +9,7 @@ const router = new Router();
 router.use('/organization', organizationRouter);
 router.use('/organizer', organizerRouter);
 router.use('/profile', profileRouter);
+router.use('/event', eventRouter);
 router.use('/user', userRouter);
 
 export default router;
--- app/routes/publicRouter.js
+++ app/routes/publicRouter.js
@@ -1,8 +1,10 @@
+import { publicEventRouter } from '../Event';
 import { passwordRouter } from '../User/profile';
 import { authRouter } from '../User';
 import Router from '../Base/Router';
 const router = new Router();
 
+router.use('/', publicEventRouter);
 router.use('/api', passwordRouter);
 router.use('/', passwordRouter);
 router.use('/', authRouter);
