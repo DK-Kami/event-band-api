acadb046b351e488b0d44a2a49a35341e5e19418
Kami <1.dankon.1@gmail.com>
2020-05-17 17:03:47 +0300

    change anonim route by get all tags

--- app/routes/publicRouter.js
+++ app/routes/publicRouter.js
@@ -5,9 +5,9 @@ import { authRouter } from '../User';
 import Router from '../Base/Router';
 const router = new Router();
 
+router.use('/tag', anonimTagRouter);
 router.use('/', publicEventRouter);
 router.use('/api', passwordRouter);
-router.use('/', anonimTagRouter);
 router.use('/', passwordRouter);
 router.use('/', authRouter);
 
