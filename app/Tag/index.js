248e9022ae38e3f920c3abf18d7e8ac76a990d06
Kami <1.dankon.1@gmail.com>
2020-05-17 23:17:08 +0300

    add comments to tag router

--- app/Tag/index.js
+++ app/Tag/index.js
@@ -4,6 +4,9 @@ import Tag from './Tag';
 const tagRouter = new Router();
 const anonimTagRouter = new Router();
 
+/**
+ * Функция для получения всех тегов
+ */
 async function getAllTagsRoute(req, res) {
   const tags = await Tag.getAll({
     order: [['name', 'ASC']],
@@ -12,9 +15,18 @@ async function getAllTagsRoute(req, res) {
   res.status(200).send({ tags });
 };
 
+/**
+ * Получене всез тегов для неавторизованного пользователя
+ */
 tagRouter.get('/all', getAllTagsRoute);
+/**
+ * Получене всез тегов для авторизованного пользователя
+ */
 anonimTagRouter.get('/all', getAllTagsRoute);
 
+/**
+ * Получене тега по его id
+ */
 tagRouter.get('/:id', async (req, res) => {
   const { id } = req.params;
 
