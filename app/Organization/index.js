353747e492563a12275b03c5165cfb682ceed7a3
Kami <1.dankon.1@gmail.com>
2020-05-18 23:02:23 +0300

    subscribe and unsubscribe on organization

--- app/Organization/index.js
+++ app/Organization/index.js
@@ -2,6 +2,7 @@ import Router from '../Base/Router';
 import Organizer from '../Organizer/Organizer';
 import Organization from './Organization';
 import User from '../User/User';
+import Subscriber from '../Subscriber/Subscriber';
 
 const organizationRouter = new Router();
 
@@ -19,6 +20,78 @@ organizationRouter.get('/:uuid', async (req, res) => {
   const { uuid } = req.params;
   const organization = await Organization.getByUUID(uuid);
   res.status(200).send({ organization });
+});
+
+/**
+ * Подписка на организацию
+ */
+organizationRouter.get('/subscribe/:uuid', async (req, res) => {
+  const { uuid: OrganizationUUID } = req.params;
+  const { userUUID } = req.payload;
+
+  const { id: UserId } = await User.getByUUID(userUUID);
+  const { id: OrganizationId } = await Organization.getByUUID(OrganizationUUID);
+
+  try {
+    const {
+      model: subscription,
+      isCreate,
+    } = await Subscriber.getOrCreate({ OrganizationId, UserId }, { status: 1 });
+
+    if (isCreate || !subscription.status) {
+      subscription.status = 1;
+      subscription.save();
+
+      res.status(201).send({
+        message: 'nice dick, awesome balls',
+        subscription,
+      });
+    }
+    else {
+      res.status(400).send({
+        message: 'email address is already in this organization',
+      });
+    }
+  }
+  catch(message) {
+    console.error(message);
+    res.status(400).send({ message });
+  }
+});
+/**
+ * Подписка на организации
+ */
+organizationRouter.get('/unsubscribe/:uuid', async (req, res) => {
+  const { uuid: OrganizationUUID } = req.params;
+  const { userUUID } = req.payload;
+
+  const { id: UserId } = await User.getByUUID(userUUID);
+  const { id: OrganizationId } = await Organization.getByUUID(OrganizationUUID);
+
+  try {
+    const subscription = await Subscriber.getOne({
+      where: { OrganizationId, UserId },
+    });
+
+    if (subscription.status) {
+      subscription.status = 0;
+      subscription.save();
+
+      res.status(201).send({
+        message: 'nice dick, awesome balls',
+        subscription,
+      });
+    }
+    else {
+      res.status(400).send({
+        message: 'you are already out of this organization',
+      });
+    }
+  }
+  catch(message) {
+    console.error(message);
+    res.status(400).send({ message });
+  }
 });
 
 /**
