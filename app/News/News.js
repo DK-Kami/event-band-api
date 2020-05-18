b6f0eb2958774a552a28e40c0325fd724d3c90b9
Kami <1.dankon.1@gmail.com>
2020-05-18 13:13:11 +0300

    News model

---
+++ app/News/News.js
@@ -0,0 +1,13 @@
+import Model from '../Base/Model';
+import models from '../../db/models';
+const {
+  News: NewsModel
+} = models;
+
+class News extends Model {
+  constructor() {
+    super(NewsModel);
+  }
+};
+
+export default new News();
