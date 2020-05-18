2400e137bc349ec816b445c0a38e6b9f014df8d6
Kami <1.dankon.1@gmail.com>
2020-05-18 13:13:30 +0300

    describe get all news route

---
+++ app/News/index.js
@@ -0,0 +1,28 @@
+import Router from '../Base/Router';
+import News from './News';
+import models from '../../db/models';
+
+const {
+  Organization: OrganizationModel,
+  User: UserModel,
+  News: NewsModel,
+} = models;
+const newsRouter = new Router();
+
+/**
+ * [DEBUG] Путь для получение всех записей
+ */
+newsRouter.get('/all', async (req, res) => {
+  const news = await News.getAll({
+    attributes: ['id', 'uuid', 'title', 'text', 'image'],
+    include: [
+      { model: OrganizationModel },
+      { model: UserModel },
+    ],
+  });
+  res.status(200).send({ news });
+});
+
+export {
+  newsRouter,
+};
