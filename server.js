75ab68e8834f738c20a32eb68f8bd46caf33e8c6
Kami <1.dankon.1@gmail.com>
2020-05-22 21:08:48 +0300

    change server.js

--- server.js
+++ server.js
@@ -1,17 +1,9 @@
-// import http from 'http';
-// import app from './app';
-
-const http = require('http');
+import app from './app';
 
 // const HOSTNAME = '127.0.0.1';
 const PORT = '8080';
 
-console.log(process.env.NODE_ENV);
-const server = http.createServer();
-server.listen('80', () => {
-  console.log('hui');
-})
-// app.listen(PORT, err => {
-//   if (err) throw err;
-//   console.log('Люди-люди, хуи на блюди');
-// });
+app.listen(PORT, err => {
+  if (err) throw err;
+  console.log('Люди-люди, хуи на блюди');
+});
