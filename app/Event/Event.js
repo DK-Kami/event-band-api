237c72e9fedb9baa82e1e0a4627d644423e46da1
Kami <1.dankon.1@gmail.com>
2020-05-24 12:51:44 +0300

    new methods

--- app/Event/Event.js
+++ app/Event/Event.js
@@ -8,6 +8,24 @@ class Event extends Model {
   constructor() {
     super(EventModel);
   }
+
+  /**
+   * Метод, возращающий массив событий в отформатированном виде
+   * @param {Array} events Массив событий, полученных ответом от сервера
+   * @param {Function} done Колбэк-функция
+   */
+  async getFormattingEvents(events, done) {
+    return events;
+  }
+
+  /**
+   * Функция, возвращающая переданное событие в отформатированном виде
+   * @param {Object} event Объект модели Event
+   * @param {Function} done колбэк-функция
+   */
+  async getFormattingEvent(event, done) {
+    return event;
+  }
 };
 
 export default new Event();
