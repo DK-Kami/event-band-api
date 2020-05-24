a78b18869f709bf23d5824832b7f3437e2754fb1
Kami <1.dankon.1@gmail.com>
2020-05-24 11:43:35 +0300

    get full info

--- app/Organization/myOrganization.js
+++ app/Organization/myOrganization.js
@@ -1,6 +1,15 @@
 import Router from "../Base/Router";
 import Organization from "./Organization";
 import User from "../User/User";
+import models from '../../db/models';
+
+const {
+  Subscriber: SubscriberModel,
+  EventTag: EventTagModel,
+  Ticket: TicketModel,
+  User: UserModel,
+  Tag: TagModel,
+} = models;
 
 const myOrganizationRouter = new Router();
 
@@ -28,15 +37,63 @@ myOrganizationRouter.use(async (req, res, next) => {
   return next();
 });
 
-myOrganizationRouter.get('/', (req, res) => {
-  const {
-    organization,
-    user,
-  } = req.payload;
+myOrganizationRouter.get('/', async (req, res) => {
+  const { organization } = req.payload;
+
+  const subscribers = await organization.getSubscribers({
+    attributes: ['id'],
+  });
+
+  const organizers = (await organization.getOrganizers({
+    attributes: ['id'],
+    include: [
+      { model: UserModel },
+    ],
+  })).map(organizer => organizer.User);
+
+  const events = (await organization.getEvents({
+    include: [
+      {
+        model: TicketModel,
+        include: [
+          {
+            model: SubscriberModel,
+            attributes: ['uuid'],
+          },
+        ],
+      },
+      {
+        model: EventTagModel,
+        attributes: ['id'],
+        include: [
+          {
+            model: TagModel,
+            attributes: ['name'],
+          },
+        ],
+      },
+    ],
+  }))
+    .map(event => ({
+      uuid: event.uuid,
+      name: event.name,
+      description: event.description,
+      datetimeTo: event.datetimeTo,
+      coords: event.coords,
+      datetimeFrom: event.datetimeFrom,
+      subscribers: event.Tickets.reduce((summ, ticket) => summ + ticket.Subscribers.length, 0),
+      count: event.Tickets.reduce((summ, ticket) => summ + ticket.count, 0),
+      minPrice: event.Tickets.filter((p, n) => p.price > n.price ? 1 : -1)[0].price,
+
+      tags: event.EventTags.map(eventTag => eventTag.Tag.name),
+      tickets: event.Tickets,
+    }));
 
   return res.status(200).send({
+    subscribers: subscribers.length,
     organization,
-    user,
+    organizers,
+    events,
   });
 });
 
