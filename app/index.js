70e5e80f54926e409db35d1bddc16a2c38f71509
Kami <1.dankon.1@gmail.com>
2020-05-27 11:22:43 +0300

    current url

--- app/index.js
+++ app/index.js
@@ -14,7 +14,11 @@ const app = express();
 app.use('/static', express.static(appDir + '/public'));
 
 const allowCrossDomain = (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8080, https://event-band-api.ru');
+  const currentUrl = proccess.env.NODE_ENV === 'production'
+    ? 'https://event-band-api.ru'
+    : 'http://localhost:8080';
+
+  res.header('Access-Control-Allow-Origin', currentUrl);
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Authorization, Content-Type, Accept");
