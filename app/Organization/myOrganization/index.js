0215f60e18486ce8104e8227647bd115adc20b2e
Kami <1.dankon.1@gmail.com>
2020-05-27 11:37:06 +0300

    add CORS to myOrganization router

--- app/Organization/myOrganization/index.js
+++ app/Organization/myOrganization/index.js
@@ -9,6 +9,19 @@ import { organizersRoter } from "./organizers";
 
 const myOrganizationRouter = new Router();
 
+const allowCrossDomain = (req, res, next) => {
+  const currentUrl = process.env.NODE_ENV === 'production'
+    ? 'https://event-band-api.ru'
+    : 'http://localhost:8080';
+
+  res.header('Access-Control-Allow-Origin', currentUrl);
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Authorization, Content-Type, Accept");
+
+  next();
+};
+myOrganizationRouter.use(allowCrossDomain);
 /**
  * Middleware, обеспечивающий безопасность путей организации,
  * путём проверки, передаваемого токена и получения организации
