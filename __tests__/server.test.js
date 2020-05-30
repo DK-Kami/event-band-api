45b1197225326eb80b2267acbc5923009df850fc
Kami <1.dankon.1@gmail.com>
2020-05-31 00:15:19 +0300

    main server test

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,12 +1,22 @@
 import supertest from 'supertest';
 import app from '../app';
 
-it('server should work', async () => {
-  await supertest(app)
-    .get(`/ping`)
-    .expect('Content-Type', /json/)
-    .expect(200)
-    .expect(({ body }) => {
-      expect(body).toHaveProperty('message', 'all ok');
-    });
+describe('server', () => {
+  it('server should work', async () => {
+    await supertest(app)
+      .get('/ping')
+      .expect(200)
+      .expect(({ body }) => {
+        expect(body).toHaveProperty('message', 'all ok');
+      });
+  });
+
+  it('server should return 500 on wrong url', async () => {
+    await supertest(app)
+      .get('/wrong')
+      .expect(500)
+      .expect(({ body }) => {
+        expect(body).toHaveProperty('message', 'method not implemented');
+      });
+  });
 });
