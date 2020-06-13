9c04740ea45671b9ddffc3e9f2b1484f6477440b
Kami <1.dankon.1@gmail.com>
2020-06-14 00:22:41 +0300

    Добавил событий

--- db/seeders/20200511135108-events.js
+++ db/seeders/20200511135108-events.js
@@ -3,6 +3,9 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Events', [
+      /**
+       * Fregalo
+      */
       {
         uuid: '7595d0f1-9836-4c9f-8002-5e310eed883d',
         name: 'Anime party',
@@ -25,6 +28,10 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date(),
       },
+
+      /**
+       * Tatar
+       */
       {
         uuid: '300921b1-4ae3-442d-8396-642cde5e565b',
         name: 'MoscowJS 47',
@@ -47,6 +54,10 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date(),
       },
+
+      /**
+       * Rock Battle
+       */
       {
         uuid: 'e9c976b2-a77c-442A-8ffd-ff2a3ff4ee6a',
         name: 'Rock battle event',
@@ -58,6 +69,109 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date(),
       },
+
+      /**
+       * VDOOH
+       */
+      {
+        uuid: 'eaf2261d-a885-4c50-b0f7-36aadda12478',
+        name: 'VDOOH Party № 1',
+        description: 'Our company organizes events of a new level',
+        datetimeFrom: '09.06.2020',
+        datetimeTo: '09.06.2020',
+        OrganizationId: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        uuid: 'c27ce1cd-8afd-43fb-9932-dbe830f452dd',
+        name: 'VDOOH Party № 2',
+        description: 'Our company organizes events of a new level',
+        datetimeFrom: '12.06.2020',
+        datetimeTo: '12.06.2020',
+        coords: '55.8398707, 37.4902621',
+        OrganizationId: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        uuid: '6c884980-23cf-4150-aec1-a9bf89be5f78',
+        name: 'VDOOH Party № 3',
+        description: 'Our company organizes events of a new level',
+        datetimeFrom: '15.06.2020',
+        datetimeTo: '15.06.2020',
+        coords: '55.8398707, 37.4902621',
+        OrganizationId: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        uuid: '80ea8dcb-4e92-4aee-9181-01c70e5c0f96',
+        name: 'VDOOH Party № 4',
+        description: 'Our company organizes events of a new level',
+        datetimeFrom: '18.06.2020',
+        datetimeTo: '18.06.2020',
+        coords: '55.8398707, 37.4902621',
+        OrganizationId: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        uuid: '0961a533-7d80-4638-914d-29f4a77d6c04',
+        name: 'VDOOH Party № 5',
+        description: 'Our company organizes events of a new level',
+        datetimeFrom: '21.06.2020',
+        datetimeTo: '21.06.2020',
+        coords: '55.8398707, 37.4902621',
+        OrganizationId: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        uuid: '6fe63fc4-3dcb-447c-ba59-644f7e3199ad',
+        name: 'VDOOH Party № 6',
+        description: 'Our company organizes events of a new level',
+        datetimeFrom: '24.06.2020',
+        datetimeTo: '24.06.2020',
+        coords: '55.8398707, 37.4902621',
+        OrganizationId: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        uuid: '867c5dd7-c419-4948-919f-0699eaed9eea',
+        name: 'VDOOH Party № 7',
+        description: 'Our company organizes events of a new level',
+        datetimeFrom: '27.06.2020',
+        datetimeTo: '27.06.2020',
+        coords: '55.8398707, 37.4902621',
+        OrganizationId: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        uuid: 'dcfbd026-b16e-496d-9895-b1303df5b9fe',
+        name: 'VDOOH Party № 8',
+        description: 'Our company organizes events of a new level',
+        datetimeFrom: '01.07.2020',
+        datetimeTo: '01.07.2020',
+        coords: '55.8398707, 37.4902621',
+        OrganizationId: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        uuid: 'f21ab991-915a-440c-ae89-fce4fa4c70fc',
+        name: 'VDOOH Party № 9',
+        description: 'Our company organizes events of a new level',
+        datetimeFrom: '04.06.2020',
+        datetimeTo: '04.06.2020',
+        coords: '55.8398707, 37.4902621',
+        OrganizationId: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+
     ], {});
   },
 
--- db/seeders/20200511135110-tickets.js
+++ db/seeders/20200511135110-tickets.js
@@ -3,6 +3,9 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Tickets', [
+      /**
+       * Fregalo
+       */
       {
         uuid: '5f16723b-52ae-4bf0-85d3-287756475cc9',
         name: 'Anime ticket',
@@ -27,6 +30,10 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date(),
       },
+
+      /**
+       * Tatar
+       */
       {
         uuid: '03b303f8-cff9-4e3d-a90f-4ba926188f53',
         name: 'Welcome to MoscowJS 47',
@@ -51,6 +58,10 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date(),
       },
+
+      /**
+       * Rock Battle
+       */
       {
         uuid: '72c1e079-6efe-4858-be2b-e898ab18957d',
         name: '1 etap',
@@ -87,6 +98,235 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date(),
       },
+
+      /**
+       * VDOOH
+       */
+      {
+        uuid: '24464059-df2a-4a1c-b0e5-47cee0502481',
+        name: 'VDOOH Party №1',
+        description: 'Ticket for VDOOH party №1',
+        count: 1000,
+        price: 500,
+        datetimeFrom: '09.06.2020',
+        datetimeTo: '09.06.2020',
+        EventId: 6,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        uuid: 'c9c623ea-9bf8-474c-9456-023cf23508d4',
+        name: 'VDOOH Party №1',
+        description: 'Free ticket for VDOOH party №1',
+        count: 200,
+        price: 0,
+        datetimeFrom: '09.06.2020',
+        datetimeTo: '09.06.2020',
+        EventId: 6,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+
+      {
+        uuid: '17fd9cb7-53c3-47e3-9df0-e5ff7e507716',
+        name: 'VDOOH Party №2',
+        description: 'Ticket for VDOOH party №2',
+        count: 1000,
+        price: 500,
+        datetimeFrom: '12.06.2020',
+        datetimeTo: '12.06.2020',
+        EventId: 7,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        uuid: '49034a99-c840-47e5-911a-9b26a6ed50c0',
+        name: 'VDOOH Party №2',
+        description: 'Free ticket for VDOOH party №2',
+        count: 200,
+        price: 0,
+        datetimeFrom: '12.06.2020',
+        datetimeTo: '12.06.2020',
+        EventId: 7,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+
+      {
+        uuid: '45729f46-6614-44fb-90ed-3262d57bade3',
+        name: 'VDOOH Party №3',
+        description: 'Ticket for VDOOH party №3',
+        count: 1000,
+        price: 500,
+        datetimeFrom: '15.06.2020',
+        datetimeTo: '15.06.2020',
+        EventId: 8,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        uuid: '5e4879e9-0c86-4e6b-b3cd-5726939258b3',
+        name: 'VDOOH Party №3',
+        description: 'Free ticket for VDOOH party №3',
+        count: 200,
+        price: 0,
+        datetimeFrom: '15.06.2020',
+        datetimeTo: '15.06.2020',
+        EventId: 8,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+
+      {
+        uuid: 'ba3b1367-9388-4adf-b91f-18f7c010fb30',
+        name: 'VDOOH Party №4',
+        description: 'Ticket for VDOOH party №4',
+        count: 1000,
+        price: 500,
+        datetimeFrom: '18.06.2020',
+        datetimeTo: '18.06.2020',
+        EventId: 9,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        uuid: '3463d4a1-2ea2-4f7e-a1a7-3afecce99d25',
+        name: 'VDOOH Party №4',
+        description: 'Free icket for VDOOH party №4',
+        count: 200,
+        price: 0,
+        datetimeFrom: '18.06.2020',
+        datetimeTo: '18.06.2020',
+        EventId: 9,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+
+      {
+        uuid: '04561c6e-83cc-4fff-a774-cfd422dc7f93',
+        name: 'VDOOH Party №5',
+        description: 'Ticket for VDOOH party №5',
+        count: 1000,
+        price: 500,
+        datetimeFrom: '21.06.2020',
+        datetimeTo: '21.06.2020',
+        EventId: 10,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        uuid: '269cac1f-72a5-4b10-b69e-a391aa97e354',
+        name: 'VDOOH Party №5',
+        description: 'Free ticket for VDOOH party №5',
+        count: 200,
+        price: 0,
+        datetimeFrom: '21.06.2020',
+        datetimeTo: '21.06.2020',
+        EventId: 10,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+
+      {
+        uuid: 'b299e8ab-e32f-471c-94c7-871b6e3db62d',
+        name: 'VDOOH Party №6',
+        description: 'Ticket for VDOOH party №6',
+        count: 1000,
+        price: 500,
+        datetimeFrom: '24.06.2020',
+        datetimeTo: '24.06.2020',
+        EventId: 11,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        uuid: 'f3bb2a25-b4b0-4e5b-ae12-c51922323132',
+        name: 'VDOOH Party №6',
+        description: 'Free ticket for VDOOH party №6',
+        count: 200,
+        price: 0,
+        datetimeFrom: '24.06.2020',
+        datetimeTo: '24.06.2020',
+        EventId: 11,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+
+      {
+        uuid: '11ffc549-04a8-43a6-afd1-b480aed5c033',
+        name: 'VDOOH Party №7',
+        description: 'Ticket for VDOOH party №7',
+        count: 1000,
+        price: 500,
+        datetimeFrom: '27.06.2020',
+        datetimeTo: '27.06.2020',
+        EventId: 12,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        uuid: 'c144036b-4e09-4e67-ab42-8f14a1914614',
+        name: 'VDOOH Party №7',
+        description: 'Free ticket for VDOOH party №7',
+        count: 200,
+        price: 0,
+        datetimeFrom: '27.06.2020',
+        datetimeTo: '27.06.2020',
+        EventId: 12,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+
+      {
+        uuid: '42a92ac2-3577-4d67-b017-c220a743e052',
+        name: 'VDOOH Party №8',
+        description: 'Ticket for VDOOH party №8',
+        count: 1000,
+        price: 500,
+        datetimeFrom: '01.07.2020',
+        datetimeTo: '01.07.2020',
+        EventId: 13,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        uuid: 'b657ae1e-7918-4eb0-917a-fc8570ddd492',
+        name: 'VDOOH Party №8',
+        description: 'Free ticket for VDOOH party №8',
+        count: 200,
+        price: 0,
+        datetimeFrom: '01.07.2020',
+        datetimeTo: '01.07.2020',
+        EventId: 13,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+
+      {
+        uuid: '1063f7a7-c449-4ac3-a1ce-674e99e453a4',
+        name: 'VDOOH Party №9',
+        description: 'Ticket for VDOOH party №9',
+        count: 1000,
+        price: 500,
+        datetimeFrom: '04.06.2020',
+        datetimeTo: '04.06.2020',
+        EventId: 14,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        uuid: '072afbc3-081f-4f6d-9d89-3abbe7de0dcf',
+        name: 'VDOOH Party №9',
+        description: 'Free ticket for VDOOH party №9',
+        count: 200,
+        price: 0,
+        datetimeFrom: '04.06.2020',
+        datetimeTo: '04.06.2020',
+        EventId: 14,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+
     ], {});
   },
 
